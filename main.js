ad75b8043df7aeb5df73262fa7368f95196e74ed
rayankhan01 <rk12172002@gmail.com>
2024-03-09 19:59:20 +0500

    Favorite Num

---
+++ main.js
@@ -0,0 +1,4 @@
+var num1 = 17;
+console.log(num1);
+var favoritenumber = 17; //
+console.log("My favorite number is ".concat(favoritenumber, ".")); //
---
+++ main.ts
@@ -0,0 +1,5 @@
+let num1 = 17
+console.log(num1);
+
+let favoritenumber: number = 17; //
+console.log(`My favorite number is ${favoritenumber}.`); //
